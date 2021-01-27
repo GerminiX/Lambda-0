e7fe742b2b91b19994daf1284fe7b6d6cb62fb7c
wiseman <wisemanska@gmail.com>
2021-01-27 21:18:17 +0200

    implemented routes initializer

--- controllers/routers/router.go
+++ controllers/routers/router.go
@@ -1,1 +1,13 @@
 package routers
+
+import (
+	"github.com/gorilla/mux"
+)
+
+func InitRoutes() *mux.Router  {
+	router := mux.NewRouter().StrictSlash(false)
+	router = SetupUserRoutes(router)
+	router = SetupTaskRoutes(router)
+	router = SetupNoteRoutes(router)
+	return router
+}
