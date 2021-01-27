34bb6be0c1649a9920aff882e72d634f7b3ccc46
wiseman <wisemanska@gmail.com>
2021-01-27 22:12:09 +0200

    implemented app configuration

--- base-common/utils.go
+++ base-common/utils.go
@@ -1,1 +1,29 @@
 package base_common
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type dbConfiguration struct {
+	Server, MongoDBHost, DBUser, DBPwd, Database string
+}
+
+func initConfig() {
+	loadConfig()
+}
+
+func loadConfig() {
+	file, err := os.Open("../etc/config.json")
+	defer file.Close()
+	if err != nil {
+		log.Fatalf("[loadConfig]: %s\n", err)
+	}
+	decoder := json.NewDecoder(file)
+	dbConfig := dbConfiguration{}
+	err = decoder.Decode(&dbConfig)
+	if err != nil {
+		log.Fatalf("[loadConfig]: %s\n", err)
+	}
+}
