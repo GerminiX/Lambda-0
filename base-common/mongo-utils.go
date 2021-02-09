60531c281532aa5d2d7ffc0a0803bafdfbb7615e
wiseman <wisemanska@gmail.com>
2021-02-09 21:08:36 +0200

    implimented session creater and getter

--- base-common/mongo-utils.go
+++ base-common/mongo-utils.go
@@ -1,1 +1,38 @@
 package base_common
+
+import (
+	"gopkg.in/mgo.v2"
+	"log"
+	"time"
+)
+
+var session *mgo.Session
+
+func GetSession() *mgo.Session {
+	if session == nil {
+		var err error
+		session, err = mgo.DialWithInfo(&mgo.DialInfo{
+			Addrs:    []string{AppConf.MongoDBHost},
+			Username: AppConf.DBUser,
+			Password: AppConf.DBPwd,
+			Timeout:  60 * time.Second,
+		})
+		if err != nil {
+			log.Fatalf("[GetSession]: %s\n", err)
+		}
+	}
+	return session
+}
+func createDbSession() {
+	var err error
+	session, err = mgo.DialWithInfo(&mgo.DialInfo{
+		Addrs:    []string{AppConf.MongoDBHost},
+		Username: AppConf.DBUser,
+		Password: AppConf.DBPwd,
+		Timeout:  60 * time.Second,
+	})
+	if err != nil {
+		log.Fatalf("[createDbSession]: %s\n", err)
+	}
+}
+
