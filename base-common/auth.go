107b38040a688273a2ee22af5f1bb71ba4515c85
wiseman <wisemanska@gmail.com>
2021-02-09 21:04:31 +0200

    implemented key init method

--- base-common/auth.go
+++ base-common/auth.go
@@ -1,1 +1,30 @@
 package base_common
+
+import (
+	"io/ioutil"
+	"log"
+)
+
+const  (
+	privKeyPath = "keys/app.rsa"
+	pubKeyPath = "keys/app.rsa.pub"
+ )
+
+var (
+	verifyKey, signKey []byte
+)
+
+func initKeys()  {
+	var err error
+	signKey, err = ioutil.ReadFile(privKeyPath)
+	if err != nil {
+		log.Fatal("[initKeys]: %s\n", err)
+	}
+
+	verifyKey, err = ioutil.ReadFile(pubKeyPath)
+	if err != nil {
+		log.Fatal("[initKeys]: %s\n", err)
+		panic(err)
+	}
+
+}
