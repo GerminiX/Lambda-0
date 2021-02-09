6e16d476625ec77b47ae93e8248f7cb3640ca3b0
wiseman <wisemanska@gmail.com>
2021-02-09 22:48:12 +0200

    implemented db indexes

--- base-common/mongo-utils.go
+++ base-common/mongo-utils.go
@@ -8,6 +8,47 @@ import (
 
 var session *mgo.Session
 
+func addIndexesToDataBase()  {
+	var err error
+	session := GetSession().Copy()
+	defer session.Close()
+	userIndex := mgo.Index{
+		Key: []string{"email"},
+		Unique: true,
+		Background: true,
+		Sparse: true,
+	}
+	taskIndex := mgo.Index{
+		Key: []string{"createdBy"},
+		Unique: false,
+		Background: true,
+		Sparse: true,
+	}
+	noteIndex := mgo.Index{
+		Key: []string{"taskId"},
+		Unique: false,
+		Background: true,
+		Sparse: true,
+	}
+
+	userCol := session.DB(AppConf.Database).C("users")
+	err = userCol.EnsureIndex(userIndex)
+	if err != nil {
+		log.Fatalf("[addUserIndexes]: %s\n", err)
+	}
+	taskCol := session.DB(AppConf.Database).C("task")
+	err = taskCol.EnsureIndex(taskIndex)
+	if err != nil {
+		log.Fatalf("[addTaskIndexes]: %s\n", err)
+	}
+	noteCol := session.DB(AppConf.Database).C("notes")
+	err = noteCol.EnsureIndex(noteIndex)
+	if err != nil {
+		log.Fatalf("[addNoteIndexes]: %s\n", err)
+	}
+
+}
+
 func GetSession() *mgo.Session {
 	if session == nil {
 		var err error
@@ -23,6 +64,7 @@ func GetSession() *mgo.Session {
 	}
 	return session
 }
+
 func createDbSession() {
 	var err error
 	session, err = mgo.DialWithInfo(&mgo.DialInfo{
@@ -35,4 +77,3 @@ func createDbSession() {
 		log.Fatalf("[createDbSession]: %s\n", err)
 	}
 }
-
