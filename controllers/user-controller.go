df7a0bb59dac3bb138d4823e60dbb90f25d54b75
wiseman <wisemanska@gmail.com>
2021-02-15 00:06:50 +0200

    implemented login handler

--- controllers/user-controller.go
+++ controllers/user-controller.go
@@ -23,7 +23,7 @@ func UserRegister(w http.ResponseWriter, r *http.Request) {
 	context := NewContext()
 	defer context.Close()
 	userCol := context.Collection(models.UserCollection)
-	userRepo := &data.UserRepository(userCol)
+	userRepo := &data.UserRepository{Col: userCol}
 	userRepo.CreateUser(user)
 	user.HashPassword = nil
 	if resp, err := json.Marshal(UserResource{Data: *user}); err != nil {
@@ -39,10 +39,63 @@ func UserRegister(w http.ResponseWriter, r *http.Request) {
 		w.Write(resp)
 	}
 
-
 }
 
 func UserLogin(w http.ResponseWriter, r *http.Request) {
+	var dataResource LoginResource
+	err := json.NewDecoder(r.Body).Decode(&dataResource)
+	if err != nil {
+		base_common.DisplayAppError(w,
+			err,
+			"Invalid login data",
+			500,
+		)
+		return
+	}
+	loginModel := dataResource.Data
+	loginUser := models.User{
+		Email: loginModel.Email,
+		Password: loginModel.Password,
+	}
+	context := NewContext()
+	defer context.Close()
+	userCol := context.Collection(models.UserCollection)
+	userRepo := &data.UserRepository{Col: userCol}
+	if user, err := userRepo.Login(loginUser); err != nil {
+		base_common.DisplayAppError(w,
+			err,
+			"Invalid login credentials",
+			401,
+		)
+		return
+	}else {
+		token, err := base_common.GenerateToken(user.Email, "member")
+		if err != nil {
+			base_common.DisplayAppError(w,
+				err,
+				"Error while generating the access token",
+				500,
+			)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		user.HashPassword = nil
+		authUser := AuthUserModel{
+			User: user,
+			Token: token,
+		}
+		resp, err := json.Marshal(AuthUserResource{Data: authUser})
+		if err != nil {
+			base_common.DisplayAppError(w,
+				err,
+				"An unexpected error has occured",
+				500,
+			)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(resp)
+	}
 
 }
 
