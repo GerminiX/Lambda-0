6c2f1bb8dd16f43b0adaf245c71964a2ebc3fbf2
wiseman <wisemanska@gmail.com>
2021-02-14 21:16:20 +0200

    implemented db context

--- controllers/context.go
+++ controllers/context.go
@@ -1,1 +1,26 @@
-package routers
+package controllers
+
+import (
+	base_common "github.com/lambda-0/base-common/base-common"
+	"gopkg.in/mgo.v2"
+)
+
+type Context struct {
+	MongoDBSession *mgo.Session
+}
+
+func NewContext() *Context {
+	session := base_common.GetSession().Copy()
+	context := &Context{
+		MongoDBSession: session,
+	}
+	return context
+}
+
+func (cont *Context) Collection(name string) *mgo.Collection  {
+	return cont.MongoDBSession.DB(base_common.AppConf.Database).C(name)
+}
+
+func (cont *Context) Close()  {
+	cont.MongoDBSession.Close()
+}
