3b0a906b462ceb8485307d1f3567a76b583796ad
wiseman <wisemanska@gmail.com>
2021-02-14 21:20:02 +0200

    created new router

--- controllers/routers/note-routers.go
+++ controllers/routers/note-routers.go
@@ -2,17 +2,20 @@ package routers
 
 import (
 	"github.com/gorilla/mux"
+	base_common "github.com/lambda-0/base-common/base-common"
+	"github.com/urfave/negroni"
 )
 
 func SetupNoteRoutes(router *mux.Router) *mux.Router {
-	router.HandleFunc("/api/v1/notes/new", controllers.createNoteHandler).Methods("POST")
-	router.HandleFunc("/api/v1/notes/{id}", controllers.updateNoteHandler).Methods("PUT")
-	router.HandleFunc("/api/v1/notes", controllers.getNotesHandler).Methods("GET")
-	router.HandleFunc("/api/v1/notes/{id}", controllers.getNoteByIdHandler).Methods("GET")
-	router.HandleFunc("/api/v1/notes/tasks/{id}", controllers.getNoteByTaskHandler).Methods("GET")
-	router.HandleFunc("/api/v1/notes/{id}", controllers.deleteNoteHandler).Methods("DELETE")
+	noteRouter := mux.NewRouter()
+	noteRouter.HandleFunc("/api/v1/notes/new", controllers.createNoteHandler).Methods("POST")
+	noteRouter.HandleFunc("/api/v1/notes/{id}", controllers.updateNoteHandler).Methods("PUT")
+	noteRouter.HandleFunc("/api/v1/notes", controllers.getNotesHandler).Methods("GET")
+	noteRouter.HandleFunc("/api/v1/notes/{id}", controllers.getNoteByIdHandler).Methods("GET")
+	noteRouter.HandleFunc("/api/v1/notes/tasks/{id}", controllers.getNoteByTaskHandler).Methods("GET")
+	noteRouter.HandleFunc("/api/v1/notes/{id}", controllers.deleteNoteHandler).Methods("DELETE")
 	router.PathPrefix("/api").Handler(negroni.New(
-		negroni.HandlerFunc(base_common.userAuthorizeHandler),
+		negroni.HandlerFunc(base_common.Authorize),
 		negroni.Wrap(noteRouter),
 	))
 	return router
--- controllers/routers/task-routers.go
+++ controllers/routers/task-routers.go
@@ -2,17 +2,20 @@ package routers
 
 import (
 	"github.com/gorilla/mux"
+	base_common "github.com/lambda-0/base-common/base-common"
+	"github.com/urfave/negroni"
 )
 
 func SetupTaskRoutes(router *mux.Router) *mux.Router {
-	router.HandleFunc("/api/v1/tasks/new", controllers.createTaskHandler).Methods("POST")
-	router.HandleFunc("/api/v1/tasks/{id}", controllers.updateTaskHandler).Methods("PUT")
-	router.HandleFunc("/api/v1/tasks", controllers.getTasksHandler).Methods("GET")
-	router.HandleFunc("/api/v1/tasks/{id}", controllers.getTaskByIdHandler).Methods("GET")
-	router.HandleFunc("/api/v1/tasks/users/{id}", controllers.getTaskByUserHandler).Methods("GET")
-	router.HandleFunc("/api/v1/tasks/{id}", controllers.deleteTaskHandler).Methods("DELETE")
+	taskRouter := mux.NewRouter()
+	taskRouter.HandleFunc("/api/v1/tasks/new", controllers.createTaskHandler).Methods("POST")
+	taskRouter.HandleFunc("/api/v1/tasks/{id}", controllers.updateTaskHandler).Methods("PUT")
+	taskRouter.HandleFunc("/api/v1/tasks", controllers.getTasksHandler).Methods("GET")
+	taskRouter.HandleFunc("/api/v1/tasks/{id}", controllers.getTaskByIdHandler).Methods("GET")
+	taskRouter.HandleFunc("/api/v1/tasks/users/{id}", controllers.getTaskByUserHandler).Methods("GET")
+	taskRouter.HandleFunc("/api/v1/tasks/{id}", controllers.deleteTaskHandler).Methods("DELETE")
 	router.PathPrefix("/api").Handler(negroni.New(
-		negroni.HandlerFunc(base_common.userAuthorizeHandler),
+		negroni.HandlerFunc(base_common.Authorize),
 		negroni.Wrap(taskRouter),
 	))
 	return router
