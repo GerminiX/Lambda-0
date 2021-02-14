337643097da49a327a67c18204a2f13cd1364af1
wiseman <wisemanska@gmail.com>
2021-02-14 22:52:06 +0200

    added user resource

--- controllers/resource.go
+++ controllers/resource.go
@@ -1,1 +1,25 @@
 package controllers
+
+import "github.com/lambda-0/base-common/models"
+
+type (
+	UserResource struct {
+		Data models.User `json:"data"`
+	}
+	LoginResource struct {
+		Data LoginModel `json:"data"`
+	}
+	AuthUserResource struct {
+		Data AuthUserModel `json:"data"`
+	}
+	LoginModel struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	AuthUserModel struct {
+		User  models.User `json:"user"`
+		Token string      `json:"token"`
+	}
+)
+
+
