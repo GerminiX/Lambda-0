fd7823cb173c6c3e10cb380a8bfb1c2faa309636
wiseman <wisemanska@gmail.com>
2021-02-14 22:51:17 +0200

    implemented users register handler

--- controllers/routers/user-routers.go
+++ controllers/routers/user-routers.go
@@ -2,10 +2,11 @@ package routers
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/lambda-0/base-common/controllers"
 )
 
 func SetupUserRoutes(router *mux.Router) *mux.Router {
-	router.HandleFunc("/users/register", controllers.userRegister).Methods("POST")
-	router.HandleFunc("/users/login", controllers.userLogin).Methods("POST")
+	router.HandleFunc("/users/register", controllers.UserRegister).Methods("POST")
+	router.HandleFunc("/users/login", controllers.UserLogin).Methods("POST")
 	return router
 }
--- controllers/user-controller.go
+++ controllers/user-controller.go
@@ -1,9 +1,48 @@
 package controllers
 
 import (
+	"encoding/json"
+	base_common "github.com/lambda-0/base-common/base-common"
+	"github.com/lambda-0/base-common/data"
+	"github.com/lambda-0/base-common/models"
 	"net/http"
 )
 
-func (controller *UsersController) userRegister(w http.Response, r *http.Request) {
-	
+func UserRegister(w http.ResponseWriter, r *http.Request) {
+	var dataResource UserResource
+	err := json.NewDecoder(r.Body).Decode(&dataResource)
+	if err != nil {
+		base_common.DisplayAppError(w,
+			err,
+			"Invalid user data",
+			500,
+		)
+		return
+	}
+	user := &dataResource.Data
+	context := NewContext()
+	defer context.Close()
+	userCol := context.Collection(models.UserCollection)
+	userRepo := &data.UserRepository(userCol)
+	userRepo.CreateUser(user)
+	user.HashPassword = nil
+	if resp, err := json.Marshal(UserResource{Data: *user}); err != nil {
+		base_common.DisplayAppError(w,
+			err,
+			"An unexpected error has occured",
+			500,
+		)
+		return
+	}else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		w.Write(resp)
+	}
+
+
 }
+
+func UserLogin(w http.ResponseWriter, r *http.Request) {
+
+}
+
