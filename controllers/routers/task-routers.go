28b6943a4585717cd2a5b8b95253d2e385895c8e
wiseman <wisemanska@gmail.com>
2021-02-15 08:25:29 +0200

    edited handlers name

--- controllers/routers/task-routers.go
+++ controllers/routers/task-routers.go
@@ -3,17 +3,18 @@ package routers
 import (
 	"github.com/gorilla/mux"
 	base_common "github.com/lambda-0/base-common/base-common"
+	"github.com/lambda-0/base-common/controllers"
 	"github.com/urfave/negroni"
 )
 
 func SetupTaskRoutes(router *mux.Router) *mux.Router {
 	taskRouter := mux.NewRouter()
-	taskRouter.HandleFunc("/api/v1/tasks/new", controllers.createTaskHandler).Methods("POST")
-	taskRouter.HandleFunc("/api/v1/tasks/{id}", controllers.updateTaskHandler).Methods("PUT")
-	taskRouter.HandleFunc("/api/v1/tasks", controllers.getTasksHandler).Methods("GET")
-	taskRouter.HandleFunc("/api/v1/tasks/{id}", controllers.getTaskByIdHandler).Methods("GET")
-	taskRouter.HandleFunc("/api/v1/tasks/users/{id}", controllers.getTaskByUserHandler).Methods("GET")
-	taskRouter.HandleFunc("/api/v1/tasks/{id}", controllers.deleteTaskHandler).Methods("DELETE")
+	taskRouter.HandleFunc("/api/v1/tasks/new", controllers.CreateTaskHandler).Methods("POST")
+	taskRouter.HandleFunc("/api/v1/tasks/{id}", controllers.UpdateTaskHandler).Methods("PUT")
+	taskRouter.HandleFunc("/api/v1/tasks", controllers.GetTasksHandler).Methods("GET")
+	taskRouter.HandleFunc("/api/v1/tasks/{id}", controllers.GetTaskByIdHandler).Methods("GET")
+	taskRouter.HandleFunc("/api/v1/tasks/users/{id}", controllers.GetTaskByUserHandler).Methods("GET")
+	taskRouter.HandleFunc("/api/v1/tasks/{id}", controllers.DeleteTaskHandler).Methods("DELETE")
 	router.PathPrefix("/api").Handler(negroni.New(
 		negroni.HandlerFunc(base_common.Authorize),
 		negroni.Wrap(taskRouter),
