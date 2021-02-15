c1e43899e243a786f9714422b9762b5dfda8a1c6
wiseman <wisemanska@gmail.com>
2021-02-15 08:30:24 +0200

    implemented create task handler

--- controllers/task-controller.go
+++ controllers/task-controller.go
@@ -1,1 +1,59 @@
 package controllers
+
+import (
+	"encoding/json"
+	base_common "github.com/lambda-0/base-common/base-common"
+	"github.com/lambda-0/base-common/data"
+	"github.com/lambda-0/base-common/models"
+	"net/http"
+)
+
+func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var dataResource TaskResource
+	err := json.NewDecoder(r.Body).Decode(&dataResource)
+	if err != nil {
+		base_common.DisplayAppError(w,
+			err,
+			"Invalid task data",
+			500,
+		)
+		return
+	}
+	task := &dataResource.Data
+	context := NewContext()
+	taskCol := context.Collection(models.TasksCollection)
+	repo := &data.TaskRepository{taskCol}
+	repo.Create(task)
+	if resp, err := json.Marshal(TaskResource{Data: *task}); err != nil {
+		base_common.DisplayAppError(w,
+			err,
+			"An unexpected error has occurred",
+			500,
+		)
+		return
+	}else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		w.Write(resp)
+	}
+}
+
+func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+func UpdateTaskHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetTaskByIdHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetTaskByUserHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
