5440046a5f6c3e56ca5585d63d821415d018be26
wiseman <wisemanska@gmail.com>
2021-02-20 18:16:06 +0200

    added task notes resource

--- controllers/resource.go
+++ controllers/resource.go
@@ -2,31 +2,48 @@ package controllers
 
 import "github.com/lambda-0/base-common/models"
 
-type (
-	UserResource struct {
-		Data models.User `json:"data"`
-	}
-	LoginResource struct {
-		Data LoginModel `json:"data"`
-	}
-	AuthUserResource struct {
-		Data AuthUserModel `json:"data"`
-	}
-	LoginModel struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
-	AuthUserModel struct {
-		User  models.User `json:"user"`
-		Token string      `json:"token"`
-	}
-)
+//user resource
+type UserResource struct {
+	Data models.User `json:"data"`
+}
 
-type (
-	TaskResource struct {
-		Data models.Task `json:"data"`
-	}
-	TasksResource struct {
-		Data []models.Task `json:"data"`
-	}
-)
+type LoginResource struct {
+	Data LoginModel `json:"data"`
+}
+
+type AuthUserResource struct {
+	Data AuthUserModel `json:"data"`
+}
+
+type LoginModel struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type AuthUserModel struct {
+	User  models.User `json:"user"`
+	Token string      `json:"token"`
+}
+
+//task resource
+type TaskResource struct {
+	Data models.Task `json:"data"`
+}
+
+type TasksResource struct {
+	Data []models.Task `json:"data"`
+}
+
+//note resource
+type NoteResource struct {
+	Data NoteModel `json:"data"`
+}
+
+type NotesResource struct {
+	Data []models.TaskNote `json:"data"`
+}
+
+type NoteModel struct {
+	TaskId      string `json:"taskId"`
+	Description string `json:"description"`
+}
